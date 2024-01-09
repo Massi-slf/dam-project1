b116744001eb4a13253ecdd9f2f56706066f04b6
Massi-slf <massinissa0888@gmail.com>
2024-01-09 14:55:01 +0100

    first com

---
+++ lib/features/Auth/presentation/pages/widgets/detail_pages.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DetailPages extends StatelessWidget {
+  const DetailPages({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
